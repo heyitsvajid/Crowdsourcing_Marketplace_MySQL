ac48c74d54c19ecbc641b56e51d73630e5f2a3fb
Vajid Kagdi <vajid9@gmail.com>
2018-03-11 23:51:37 -0700

    Mocha Test cases
    7 unit test cases using mocha

---
+++ test/test.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var request = require('request');
+var assert = require('assert');
+var http = require('http');
+var mocha = require('mocha');
+
+
+it('Test Login with wrong Credentials', function (done) {
+    request.post('http://localhost:3001/login',
+        { form: { uname: 'shreykbhatt', password: 'test' } },
+        function (error, response, body) {
+            //console.log(response.body);
+            assert.equal(400, response.statusCode);
+            done();
+        });
+});
+
+it('Test Login with Right Credentials', function (done) {
+    request.post('http://localhost:3001/login',
+        { form: { email: 'vajid9@gmail.com', password: '123456' } },
+        function (error, response, body) {
+            //console.log(response.body);
+            assert.equal(200, response.statusCode);
+            done();
+        });
+});
+
+it('Check is logged in', function (done) {
+    request.get('http://localhost:3001/isLoggedIn',
+
+        function (error, response, body) {
+            //console.log(response.body);
+            assert.equal(200, response.statusCode);
+            done();
+        });
+});
+
+it('Get user projects', function (done) {
+    request.post('http://localhost:3001/getUserProjects',
+        { form: { id: 1 } },
+        function (error, response, body) {
+            //console.log(response.body);
+            assert.equal(200, response.statusCode);
+            done();
+        });
+});
+
+it('Check bid for particular user and project', function (done) {
+    request.post('http://localhost:3001/checkBid',
+        { form: { projectId: 1, userId: 1 } },
+        function (error, response, body) {
+            //console.log(response.body);
+            assert.equal(200, response.statusCode);
+            done();
+        });
+});
+
+it('Get user bid projects', function (done) {
+    request.post('http://localhost:3001/getUserBidProjects',
+        { form: { id: 1 } },
+        function (error, response, body) {
+            //console.log(response.body);
+            assert.equal(200, response.statusCode);
+            done();
+        });
+});
+
+it('Get bids for project id', function (done) {
+    request.post('http://localhost:3001/getBids',
+        { form: { id: 6 } },
+        function (error, response, body) {
+            //console.log(response.body);
+            assert.equal(200, response.statusCode);
+            done();
+        });
+});
